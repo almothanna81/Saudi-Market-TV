38251078410f9f691f1004a76cc0def0732f3be0
almothanna81 <almothanna@gmail.com>
2026-08-10 17:33:10 +0300

    Bump version to V2.0 dashboard

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.almothanna.saudimarkettv"
         minSdk = 23
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "2.0"
     }
 
     buildTypes {
