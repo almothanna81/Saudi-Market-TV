42151234953f0c1523a27c4ae18679b9e86c1fb8
almothanna81 <almothanna@gmail.com>
2026-08-10 16:31:46 +0300

    Add Android app Gradle configuration

---
+++ app/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.almothanna.saudimarkettv"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.almothanna.saudimarkettv"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
