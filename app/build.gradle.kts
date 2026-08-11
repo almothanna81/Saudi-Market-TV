e9a0ff58597c29cc164229f4b480d505ac72564f
almothanna81 <almothanna@gmail.com>
2026-08-11 13:49:25 +0300

    Bump version to 2.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.almothanna.saudimarkettv"
         minSdk = 23
         targetSdk = 35
-        versionCode = 9
-        versionName = "2.5"
+        versionCode = 10
+        versionName = "2.6"
     }
 
     buildTypes {
